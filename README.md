44009c750353ba35317cd479ed3157469179f5f2
Greg Look <greg@amperity.com>
2019-08-06 17:31:01 -0700

    Initial project skeleton.

---
+++ README.md
@@ -0,0 +1,16 @@
+gocd-aurora-elastic-agent
+=========================
+
+A plugin for [GoCD](https://www.gocd.org/) providing elastic agent support via
+[Apache Aurora](https://aurora.apache.org/).
+
+
+## Usage
+
+FIXME
+
+
+## License
+
+Licensed under the Apache License, Version 2.0. See the [LICENSE](LICENSE) file
+for rights and restrictions.
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject amperity/gocd-aurora-elastic-agent "0.1.0-SNAPSHOT"
+  :description "A plugin for GoCD providing elastic agent support via Apache Aurora."
+  :url "https://github.com/amperity/gocd-aurora-elastic-agent"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+
+  :dependencies
+  [[org.clojure/clojure "1.10.1"]])
