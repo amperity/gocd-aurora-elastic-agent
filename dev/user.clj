35d364956f7af38cc69ed72e602513225bdf2789
Greg Look <greg@amperity.com>
2019-08-08 12:58:28 -0700

    Redirect agent logs to console.

--- dev/user.clj
+++ dev/user.clj
@@ -3,6 +3,12 @@
     [amperity.gocd.agent.aurora.client :as aurora]
     [amperity.gocd.agent.aurora.plugin :as plugin]
     [amperity.gocd.agent.aurora.util :as u]
+    [clojure.java.io :as io]
     [clojure.repl :refer :all]
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]))
+
+
+(def state
+  "Placeholder atom, representing the plugin state."
+  (atom {}))
--- src/amperity/gocd/agent/aurora/client.clj
+++ src/amperity/gocd/agent/aurora/client.clj
@@ -2,6 +2,7 @@
   "Aurora client methods and wrappers."
   (:require
     [amperity.gocd.agent.aurora.util :as u]
+    [clojure.java.io :as io]
     [clojure.string :as str])
   (:import
     (org.apache.aurora.gen
@@ -61,8 +62,9 @@
   "Constructs a new Aurora process definition to fetch and install the gocd
   agent."
   [params]
-  (let [autoregister-properties-path "go-agent/config/autoregister.properties"
-        wrapper-properties-path "go-agent/wrapper-config/wrapper-properties.conf"]
+  (let [wrapper-properties-path "go-agent/wrapper-config/wrapper-properties.conf"
+        autoregister-properties-path "go-agent/config/autoregister.properties"
+        logback-xml (slurp (io/resource "amperity/gocd/agent/aurora/logback-agent.xml"))]
     (letfn [(clean
               [path]
               (str "rm -f " path))
@@ -78,17 +80,21 @@
        :ephemeral false
        :min_duration 5
        :cmdline (str/join
-                  " && "
-                  ["mkdir go-agent/config"
+                  "\n"
+                  ["set -e"
+                   "mkdir go-agent/config"
+                   (clean wrapper-properties-path)
+                   (wrapper-property "wrapper.app.parameter.100" "-serverUrl")
+                   (wrapper-property "wrapper.app.parameter.101" (:server-url params))
                    (clean autoregister-properties-path)
                    (autoregister-property "agent.auto.register.key" (:auto-register-key params))
                    (autoregister-property "agent.auto.register.hostname" (:auto-register-hostname params))
                    (autoregister-property "agent.auto.register.environments" (:auto-register-environment params))
                    (autoregister-property "agent.auto.register.elasticAgent.pluginId" (:elastic-plugin-id params))
                    (autoregister-property "agent.auto.register.elasticAgent.agentId" (:elastic-agent-id params))
-                   (clean wrapper-properties-path)
-                   (wrapper-property "wrapper.app.parameter.100" "-serverUrl")
-                   (wrapper-property "wrapper.app.parameter.101" (:server-url params))])
+                   (str "base64 -d <<<'" (u/b64-encode-str logback-xml) "' > go-agent/config/agent-bootstrapper-logback.xml")
+                   "cp go-agent/config/agent-bootstrapper-logback.xml go-agent/config/agent-launcher-logback.xml"
+                   "cp go-agent/config/agent-bootstrapper-logback.xml go-agent/config/agent-logback.xml"])
        :final false})))
 
 
