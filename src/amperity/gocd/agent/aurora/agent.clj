5607227644ee6ff8e45efdffae010c663bdc0085
Greg Look <greg@amperity.com>
2019-08-10 11:35:02 -0700

    Add agent id and resource functions.

--- src/amperity/gocd/agent/aurora/agent.clj
+++ src/amperity/gocd/agent/aurora/agent.clj
@@ -69,24 +69,66 @@
               :message "Could not parse disk allocation as an integer"}))))]))
 
 
-(defn profile->resources
-  "Construct a map of required Aurora resources for an elastic agent profile."
-  [profile]
-  {:cpu (let [cpu (:cpu profile)]
-          (if-not (str/blank? cpu)
-            (Double/parseDouble cpu)
-            1.0))
-   :ram (let [ram (:ram profile)]
-          (if-not (str/blank? ram)
-            (Integer/parseInt ram)
-            1024))
-   :disk (let [disk (:disk profile)]
-           (if-not (str/blank? disk)
-             (Integer/parseInt disk)
-             1024))})
-
-
 
 ;; ## Agent Identifiers
 
-,,,
+(defn form-id
+  "Form an agent identifier from a map of the aurora cluster, role, and env,
+  and the agent name."
+  ([params]
+   (form-id
+     (:aurora-cluster params)
+     (:aurora-role params)
+     (:aurora-env params)
+     (:agent-name params)))
+  ([aurora-cluster aurora-role aurora-env agent-name]
+   (str/join "/" [aurora-cluster aurora-role aurora-env agent-name])))
+
+
+(defn parse-id
+  "Parse an agent identifier, returning a map of the aurora cluster, role, and
+  env, and the agent name."
+  [agent-id]
+  (let [[cluster role env name] (str/split agent-id #"/")]
+    {:aurora-cluster cluster
+     :aurora-role role
+     :aurora-env env
+     :agent-name name}))
+
+
+
+;; ## Resources
+
+(def default-resources
+  "Default set of resources to provide agent jobs."
+  {:cpu 1.0
+   :ram 512
+   :disk 1024})
+
+
+(defn profile->resources
+  "Construct a map of required Aurora resources for an elastic agent profile."
+  [profile]
+  (into {}
+        (filter val)
+        {:cpu (let [cpu (:cpu profile)]
+                (when-not (str/blank? cpu)
+                  (Double/parseDouble cpu)))
+         :ram (let [ram (:ram profile)]
+                (when-not (str/blank? ram)
+                  (Integer/parseInt ram)))
+         :disk (let [disk (:disk profile)]
+                 (when-not (str/blank? disk)
+                   (Integer/parseInt disk)))}))
+
+
+(defn resource-satisfied?
+  "True if the resources required in profile `a` are satisfied by an agent with
+  profile `b`."
+  [a b]
+  (every?
+    (fn check-resource
+      [[resource requirement]]
+      (when-let [available (get b resource)]
+        (<= requirement available)))
+    a))
