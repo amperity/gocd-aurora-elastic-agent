45010957ac73fd9b15e4d19e4ae4a9515a3e6202
Greg Look <greg@amperity.com>
2019-08-19 21:48:26 -0700

    Quieter log for transport exceptions.

--- src/amperity/gocd/agent/aurora/scheduler.clj
+++ src/amperity/gocd/agent/aurora/scheduler.clj
@@ -731,6 +731,10 @@
     (try
       (let [client (get-cluster-client scheduler cluster-profile)]
         (aurora/list-agents client cluster-profile))
+      (catch org.apache.thrift.transport.TTransportException ex
+        (log/error "Failed to list aurora agents for cluster %s: %s"
+                    (:aurora_cluster cluster-profile)
+                    (.getMessage ex)))
       (catch Exception ex
         (log/errorx ex "Failed to list aurora agents for cluster %s"
                     (:aurora_cluster cluster-profile))
