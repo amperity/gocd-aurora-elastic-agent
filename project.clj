dd96a0fe316ecabe3cd7fd6c8886951bae097d04
Drew Inglis <drew@drewinglis.com>
2020-09-24 13:41:57 -0700

    0.2.2-SNAPSHOT version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amperity/gocd-aurora-elastic-agent "0.2.1"
+(defproject amperity/gocd-aurora-elastic-agent "0.2.2-SNAPSHOT"
   :description "A plugin for GoCD providing elastic agent support via Apache Aurora."
   :url "https://github.com/amperity/gocd-aurora-elastic-agent"
   :license {:name "Apache License 2.0"
